b4a235db6134c7fe09dd3287c6f1d3d93832741b
Muhammad Usman <tmusman5@gmail.com>
2023-07-22 17:20:32 +0500

    changes: user create and user response schemas
    refactor: spelling mistake in created_at column

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 from datetime import datetime
 
@@ -15,7 +15,22 @@ class PostCreate(PostBase):
 
 class Post(PostBase):
     id: int
-    create_at: datetime
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+
+    id: int
+    email: EmailStr
+    created_at: datetime
 
     class Config:
         orm_mode = True
