66a03a757fb496388020b91f9ffd26d4de9b0ee0
Muhammad Usman <tmusman5@gmail.com>
2023-08-17 12:39:31 +0500

    changes: postgres schema for vote

--- app/schemas.py
+++ app/schemas.py
@@ -1,25 +1,5 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
+from pydantic import BaseModel, EmailStr, conint
 from datetime import datetime
-
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-class PostCreate(PostBase):
-    pass
-
-
-class Post(PostBase):
-    id: int
-    created_at: datetime
-    owner_id: int
-
-    class Config:
-        orm_mode = True
 
 
 class UserCreate(BaseModel):
@@ -42,6 +22,26 @@ class UserLogin(BaseModel):
     password: str
 
 
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -49,3 +49,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: int
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
