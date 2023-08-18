f619ff019d52a3aa60e9e967c816e0f9d9cc45a3
Muhammad Usman <tmusman5@gmail.com>
2023-08-19 00:09:56 +0500

    add: phone number column
    commit includes alembic version and column in User model

--- app/models.py
+++ app/models.py
@@ -30,6 +30,8 @@ class User(Base):
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text("now()"))
 
+    phone_number = Column(String)
+
 
 class Vote(Base):
     __tablename__ = "votes"
