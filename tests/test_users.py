9270e8b41264813177088889dd55dfe9cdb6756b
Muhammad Usman <tmusman5@gmail.com>
2023-09-06 23:30:06 +0500

    changes: tests for create_user and user login

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,25 @@
-from fastapi.testclient import TestClient
-from app.main import app
+from .database import client, session
+from app import schemas
 
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     resp = client.get("/")
     # print(resp.json().get("message"))
+    
     assert resp.json().get("message") == "Hello World"
     assert resp.status_code == 200
+
+def test_create_user(client):
+    resp = client.post("/users", json={"email":"test@gmail.com", "password":"123"})
+    # print("RESPONSE: ",resp.json())
+    new_user = schemas.UserOut(**resp.json())
+
+    assert new_user.email == "test@gmail.com"
+    assert resp.status_code == 201
+
+def test_login_user(client):
+
+    resp = client.post("/login", data={"username": "test@gmail.com", "password": "123"})
+
+    assert resp.status_code == 200
+    
+
